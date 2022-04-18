9b655081da2d23b7e705b055a47f8be974338012
irina.grechko <grechko.irinka@gmaill.com>
2022-04-18 10:52:51 +0300

    add profile form

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,15 @@
 export const LOG_IN = 'LOG_IN'
 export const LOG_OUT = 'LOG_OUT'
 export const AUTHENTICATE = 'AUTHENTICATE'
+export const SET_CARD = 'SET_CARD'
+export const GET_CARD = 'GET_CARD'
+export const FETCH_CARD = 'FETCH_CARD'
+export const REGISTRATION = 'REGISTRATION'
 
 export const logIn = () => ({type: LOG_IN})
 export const logOut = () => ({type: LOG_OUT})
 export const authenticate = (email, password) => ({type: AUTHENTICATE, payload: {email, password}})
+export const setCardInfo = () => ({type: SET_CARD})
+export const fetchCardInfo = (cardInfo) => ({ type: FETCH_CARD, payload: {cardInfo}})
+export const getCardInfo = (authToken) => ({ type: GET_CARD, payload: {authToken}})
+
--- src/api.js
+++ src/api.js
@@ -2,3 +2,13 @@ export const serverLogin = async (email, password) => {
   return fetch(`https://loft-taxi.glitch.me/auth?username=${email}&password=${password}`
   ).then(res => res.json()).then(data => data.success)
 }
+
+export const setCardInfo = async (cardInfo, authToken) => {
+  const {cardNumber, expiryDate, cardName, cvc} = cardInfo;
+  return fetch(`https://loft-taxi.glitch.me/card?cardNumber=${cardNumber}&expiryDate=${expiryDate}&
+  cardName=${cardName}&cvc=${cvc}&token=${authToken}`).then(res => res.json()).then(data => data.success)
+}
+
+export const getCardInfo = async (authToken) => {
+  return fetch(`https://loft-taxi.glitch.me/card?token=${authToken}`).then(res => res.json()).then(data => data.success)
+}
